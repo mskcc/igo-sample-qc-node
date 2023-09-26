2a508e969cc4b8010c839e3435421f72d41c3c8f
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-09-26 16:35:34 -0400

    dev db config

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,7 +1,16 @@
+// module.exports = {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '0rangeP0tat03',
+//     database: 'dev_sample_qc',
+//     dialect: 'mysql', 
+// };
+
+//DEV
 module.exports = {
     host: 'localhost',
-    user: 'root',
-    password: '0rangeP0tat03',
+    user: 'sample_qc_dev_user',
+    password: 'napkinWatermelon1Dev',
     database: 'dev_sample_qc',
     dialect: 'mysql', 
 };
