b28e91b564c62d8683130bff42c013cf75b3f079
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-11-07 16:18:22 -0500

    try to fix logging

--- backend/util/apiResponse.js
+++ backend/util/apiResponse.js
@@ -2,10 +2,20 @@ const winston = require('winston');
 const myWinstonOptions = {
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'SampleQC_combined.log' })
+        new winston.transports.File({ filename: 'SampleQC_logs.log' })
     ]
 };
 const logger = new winston.createLogger(myWinstonOptions);
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+// if (process.env.NODE_ENV !== 'production') {
+//     logger.add(new winston.transports.Console({
+//       format: winston.format.simple(),
+//     }));
+//   }
 
 exports.successResponse = function (res, msg) {
     var data = {
@@ -26,6 +36,10 @@ exports.successResponseWithData = function (res, msg, data) {
             user: user,
         },
     };
+    logger.log({
+        level: 'info',
+        message: 'WE ARE HITTING THIS'
+    });
     res.status(200).json(resData);
 };
 
