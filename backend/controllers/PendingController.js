9bbb6cc30089ca5538aae4684bf792b18df249e9
Stephanie DelBelso <sdelbelso@grubhub.com>
2024-03-07 12:46:21 -0500

    pending list logic

--- backend/controllers/PendingController.js
+++ backend/controllers/PendingController.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const CommentRelation = db.commentRelations;
+const { Op } = require('sequelize');
 const Comments = db.comments;
 const Users = db.users;
 const apiResponse = require('../util/apiResponse');
@@ -8,7 +9,6 @@ const { buildPendingList } = require('../util/helpers');
 
 exports.getPendingRequests = [
     function(req, res) {
-        console.log(res.user);
         const userType = req.params.userRole;
         const responseData = [];
         
@@ -18,84 +18,96 @@ exports.getPendingRequests = [
             if (!pendingResponse || pendingResponse.length === 0) {
                 return apiResponse.errorResponse(res, 'No pending request data available.');
             }
-            
+
             const [pendingRequests] = pendingResponse;
+
+            // build where clause conditions
+            const conditions = [];
             Object.keys(pendingRequests).forEach(pendingRequestId => {
-                // console.log(pendingResponseData[pendingRequestId]);
-                CommentRelation.findOne({
-                    where: {
-                        request_id: pendingRequestId,
-                        report: pendingRequests[pendingRequestId]
-                    }
-                }).then(commentRelationRecord => {
-                    if (commentRelationRecord && commentRelationRecord.length > 0) {
-                        // return Comments.findAll({
-                        //     where: {
-                        //         commentrelation_id: commentRelationRecord.id
-                        //     },
-                        //     order: [['date_created', 'DESC']]
-                        // }).then(allReportComments => {
+                conditions.push({
+                    request_id: pendingRequestId,
+                    report: pendingRequests[pendingRequestId]
+                });
+            });
+
+                
+            CommentRelation.findAll({
+                where: {
+                    [Op.or]: conditions
+                }
+            }).then(commentRelationRecords => {
+                
+                if (commentRelationRecords && commentRelationRecords.length > 0) {
+                    commentRelationRecords.forEach(record => {
+                        const isAuthed = userType === 'lab_member'; // || isAuthorizedForRequest()
+
+                        if (isAuthed) {
                             if (userType === 'user') {
                                 responseData.push({
-                                    request_id: pendingRequestId,
-                                    report: commentRelationRecord.report,
-                                    date: commentRelationRecord.createdAt.toLocaleString(),
+                                    request_id: record.request_id,
+                                    report: record.report,
+                                    date: record.createdAt.toLocaleString(),
                                     // most_recent_date: allReportComments[0].createdAt.toLocaleString(),
-                                    show: `<span pending-id='${pendingRequestId}' class ='show-icon'><i class='material-icons'>forward</i></span>`
+                                    show: `<span pending-id='${record.request_id}' class ='show-icon'><i class='material-icons'>forward</i></span>`
                                 });
                             } else {
-                                //lab_member or PM
-
-                                // let labNotifications = 0;
-                                // let PmNotifications = 0;
-                                // let userReplies = 0;
-                                // allReportComments.forEach(comment => {
-                                //     Users.findOne({
-                                //         where: {
-                                //             username: comment.username
-                                //         }
-                                //     }).then(user => {
-                                //         if (user.role === 'lab_member') {
-                                //             labNotifications = labNotifications + 1;
-                                //         } else if (user.role === 'cmo_pm') {
-                                //             PmNotifications = PmNotifications + 1;
-                                //         } else if (user.role === 'user') {
-                                //             userReplies = userReplies + 1;
-                                //         }
-                                //     });
-                                // });
                                 responseData.push({
-                                    request_id: pendingRequestId,
-                                    report: commentRelationRecord.report,
-                                    date: commentRelationRecord.createdAt.toLocaleString(),
+                                    request_id: record.request_id,
+                                    report: record.report,
+                                    date: record.createdAt.toLocaleString(),
                                     // most_recent_date: allReportComments[0].createdAt.toLocaleString(),
-                                    author: commentRelationRecord.author,
-                                    recipients: commentRelationRecord.recipients,
+                                    author: record.author,
+                                    recipients: record.recipients,
                                     // lab_notifications: labNotifications,
                                     // pm_notifications: PmNotifications,
                                     // user_replies: userReplies,
-                                    show: `<span pending-id='${pendingRequestId}' class ='show-icon'><i class='material-icons'>forward</i></span>`
+                                    show: `<span pending-id='${record.request_id}' class ='show-icon'><i class='material-icons'>forward</i></span>`
                                 });
-                            }
+                        }
+                    }})
+                    // return Comments.findAll({
+                    //     where: {
+                    //         commentrelation_id: commentRelationRecord.id
+                    //     },
+                    //     order: [['date_created', 'DESC']]
+                    // }).then(allReportComments => {
+                        
+                            //lab_member or PM
+
+                            // let labNotifications = 0;
+                            // let PmNotifications = 0;
+                            // let userReplies = 0;
+                            // allReportComments.forEach(comment => {
+                            //     Users.findOne({
+                            //         where: {
+                            //             username: comment.username
+                            //         }
+                            //     }).then(user => {
+                            //         if (user.role === 'lab_member') {
+                            //             labNotifications = labNotifications + 1;
+                            //         } else if (user.role === 'cmo_pm') {
+                            //             PmNotifications = PmNotifications + 1;
+                            //         } else if (user.role === 'user') {
+                            //             userReplies = userReplies + 1;
+                            //         }
+                            //     });
+                            // });
+                    let pendingTable = {};
+                    if (userType === 'lab_member' || userType === 'cmo_pm') {
+                        pendingTable = buildPendingList(responseData, false);
+                    } else {
+                        // userType === 'user'
+                        pendingTable = buildPendingList(responseData, true);
                     }
-                }).catch(e => {
-                    console.log(e);
-                    return apiResponse.errorResponse(res, `ERROR querying MySQL database: ${e}`);
-                });
-            
+
+                    return apiResponse.successResponseWithData(res, 'success', pendingTable);
+                }
+                return apiResponse.successResponseWithData(res, 'No pending request data found in database.', {});
+
+            }).catch(e => {
+                console.log(e);
+                return apiResponse.errorResponse(res, `ERROR querying MySQL database: ${e}`);
             });
-
-            let pendingTable = {};
-            if (userType === 'lab_member' || userType === 'cmo_pm') {
-                pendingTable = buildPendingList(responseData, false);
-            } else {
-                // userType === 'user'
-                pendingTable = buildPendingList(responseData, true);
-            }
-
-            return apiResponse.successResponseWithData(res, 'success', pendingTable);
-
-
         }).catch(e => {
             console.log(e);
             return apiResponse.errorResponse(res, `ERROR querying LIMS: ${e}`);
