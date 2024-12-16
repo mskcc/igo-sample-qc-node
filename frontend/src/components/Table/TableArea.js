9482e6135c7966011f82c9d011c6836dcb499715
Mirhaj <mirhajf@lski2475.mskcc.org>
2024-12-16 10:59:53 -0500

    Recipe props reference fix

--- frontend/src/components/Table/TableArea.js
+++ frontend/src/components/Table/TableArea.js
@@ -114,7 +114,7 @@ export default function TableArea(props) {
   const isInvestigatorPrepped = props.report.request.requestName === 'Investigator Prepared Libraries' ||
             props.report.request.requestName === 'Investigator Prepared Pools';
 
-  const shouldDisplayCellInfo = props.report.table.recipe === 'OGM' || props.report.table.recipe.includes("ATAC") || props.report.table.recipe.includes("SC");
+  const shouldDisplayCellInfo = props.report.tables.recipe === 'OGM' || props.report.tables.recipe.includes("ATAC") || props.report.tables.recipe.includes("SC");
 
   return (
     <div className={classes.container}>
