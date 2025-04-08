8e925fa9a31a2393e6a6ae0e0150bfb539d96db4
Ojasvi1329 <ojasvipatel113@gmail.com>
2025-04-08 13:20:50 -0400

    Remove Instructions from Sample QC

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,12 +1,10 @@
 import Header from './Header.js';
-//import Instructions from './Instructions.js';
 import Message from './Message.js';
 import SnackMessage from './SnackMessage.js';
 import PendingTable from './Pending/PendingTable.js';
 
 export {
   Header,
- // Instructions,
   Message,
   PendingTable,
   SnackMessage,
